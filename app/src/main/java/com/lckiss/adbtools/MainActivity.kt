e0a2129774714b304ec7b07a2cd0c018201a2630
Anr-C <ilckiss@foxmail.com>
2020-11-14 17:27:50 +0800

    remove unused package import.

--- app/src/main/java/com/lckiss/adbtools/MainActivity.kt
+++ app/src/main/java/com/lckiss/adbtools/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.lckiss.adbtools
 
-import android.animation.Animator
 import android.content.Context
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
-import android.view.View
-import android.view.ViewAnimationUtils
 import android.widget.CompoundButton
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
